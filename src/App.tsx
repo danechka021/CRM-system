dc4855ddaf5989c9357cd6700bb3a9f5d33d2f45
Даня <daniamitin35@gmail.com>
2026-03-28 16:42:40 +0300

    ED-1785: Сдела дизайн макета авторизации через CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
-import AppLayout from "./AppLayout/AppLayout";
+//import AppLayout from "./AppLayout/AppLayout";
+import AuthorizationPage from "./pages/auth/AuthorizationPage";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppLayout />
+      <AuthorizationPage />
     </BrowserRouter>
   );
 };
